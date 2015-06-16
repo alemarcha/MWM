caec0d403bfbe000be48e1b9e0aafe57e7a3033c
Alexis Martínez <alemarcha26@MacBook-Pro-de-Alexis.local>
2015-06-16 20:20:35 +0200

    Initial Commit

---
+++ MasterApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MasterApp
+//
+//  Created by Alexis Martínez on 16/6/15.
+//  Copyright (c) 2015 Alexis Martínez Chacón. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MasterApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MasterApp
+//
+//  Created by Alexis Martínez on 16/6/15.
+//  Copyright (c) 2015 Alexis Martínez Chacón. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
